432ae93b3df070f63b3f5fb430352e9d4138f694
shubhamofficial2486 <shubhamofficial2486@gmail.com>
2025-04-04 18:59:51 +0530

    Checking Purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 import './App.css';
-import Layout from './components/DuncoLayout';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Layout/>
+      <Header/>
     </div>
   );
 }
